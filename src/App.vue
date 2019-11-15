55d0770530b3f7d336299259e92a9062517f91bf
Maxime Moreillon <m.moreillon@gmail.com>
2019-11-15 09:25:55 +0900

    Swtiched to new template

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,32 @@
 <template>
   <div id="app">
-    <Header applicationName="申請マネージャー"/>
-
-    <div class="columns_container">
-      <Nav>
-        <router-link to="create_application">
-          <span class="mdi mdi-file-document-box-plus-outline"/>
-          <span>New</span>
-        </router-link>
-        <router-link to="/">
-          <span class="mdi mdi-file-document-box-multiple-outline"/>
-          <span>Applications</span>
-        </router-link>
-        <router-link to="/received_applications">
-          <span class="mdi mdi-inbox-arrow-down"/>
-          <span>Inbox</span>
-        </router-link>
-      </Nav>
-
-      <div class="right_column">
-        <router-view class="main"/>
-        <Footer applicationName="申請マネージャー v0.3"/>
-      </div>
-
-    </div>
+    <AppTemplate
+      applicationName="申請マネージャー"
+      v-bind:navigation="navigation"
+      />
   </div>
 </template>
 
 <script>
-import Header from '@/components/jtekt_vue_layouting_components/Header.vue'
-import Footer from '@/components/jtekt_vue_layouting_components/Footer.vue'
-import Nav from '@/components/jtekt_vue_layouting_components/Nav.vue'
+import AppTemplate from '@/components/jtekt_vue_template/AppTemplate.vue'
+
 
 export default {
   components: {
-    Header,
-    Footer,
-    Nav,
+    AppTemplate
   },
+  data(){
+    return{
+      navigation : [
+        {route: '/create_application', label: 'New', icon: 'file-document-box-plus-outline'},
+        {route: '/', label: 'Outbox', icon: 'inbox-arrow-up'},
+        {route: '/received_applications', label: 'Inbox', icon: 'inbox-arrow-down'},
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-
-@import './components/jtekt_vue_layouting_components/master.css';
-
-.main {
-  padding: 25px;
-}
-
-
-
-
-
 
 </style>
