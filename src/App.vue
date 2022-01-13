e5d0b2cc26fb92b959c94be58771a11b69ea0fce
Maxime Moreillon <m.moreillon@gmail.com>
2022-01-13 10:01:21 +0900

    env

--- src/App.vue
+++ src/App.vue
@@ -52,11 +52,9 @@ export default {
       options: {
         title: "申請マネージャー (Vuetify)",
         skip_greetings: process.env.NODE_ENV === 'development',
-        login_url: `${process.env.VUE_APP_EMPLOYEE_MANAGER_URL}/v2/auth/login`,
-        identification_url: `${process.env.VUE_APP_EMPLOYEE_MANAGER_URL}/v2/users/self`,
+        login_url: `${process.env.VUE_APP_EMPLOYEE_MANAGER_API_URL}/v2/auth/login`,
+        identification_url: `${process.env.VUE_APP_EMPLOYEE_MANAGER_API_URL}/v2/users/self`,
         password_reset_url: `${process.env.VUE_APP_EMPLOYEE_MANAGER_FRONT_URL}/password_reset`,
-        main_class: 'grey lighten-4',
-        footer_color: 'grey lighten-4',
       },
 
     }
