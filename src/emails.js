6c48c590b72b74027ce584fdeffe370ebc6e7a25
Maxime Moreillon <m.moreillon@gmail.com>
2022-08-05 07:38:24 +0900

    cleanup of emails

---
+++ src/emails.js
@@ -0,0 +1,46 @@
+const application_is_rejected = (application) => {
+    return !!application.recipients.find(recipient => recipient.refusal)
+}
+
+const generate_email_to_recipient_body = (application, recipient) =>
+`${recipient.display_name} 様
+
+申請マネージャーの通知メールです。
+
+申請を提出しました。
+
+申請者: ${application.applicant.display_name}
+タイプ: ${application.type}
+タイトル: ${application.title}
+提出先URL: ${window.location.origin}/applications/${application._id}
+
+確認お願いします。`
+
+const generate_email_to_recipient = (application, recipient) =>
+`mailto:${recipient.email_address}
+?subject=[申請マネージャ] ${application.type}
+&body=${encodeURIComponent(generate_email_to_recipient_body(application, recipient))}`
+
+const generate_email_to_applicant_body = (application) =>
+`${application.applicant.display_name} 様
+
+申請マネージャーの通知メールです。
+
+申請の承認が${application_is_rejected(application) ? '却下' : '完了'}されました。
+
+申請者: ${application.applicant.display_name}
+タイプ: ${application.type}
+タイトル: ${application.title}
+提出先URL: ${window.location.origin}/applications/${application._id}
+
+確認お願いします。`
+
+const generate_email_to_applicant = (application) =>
+`mailto:${application.applicant.email_address}
+?subject=[申請マネージャ] ${application.type}
+&body=${encodeURIComponent(generate_email_to_applicant_body(application))}`
+
+export {
+    generate_email_to_applicant,
+    generate_email_to_recipient,
+}
--- src/views/Application.vue
+++ src/views/Application.vue
@@ -178,10 +178,6 @@
                       </v-btn>
                     </template>
 
-
-
-
-
                   </v-list-item-content>
 
                   <v-list-item-content class="align-end" v-else-if="field.type === 'file'">
@@ -302,288 +298,245 @@
 </template>
 
 <script>
-  import HelpDialog from '@/components/HelpDialog.vue'
-  import IdUtils from '@/mixins/IdUtils.js'
+import HelpDialog from '@/components/HelpDialog.vue'
+import IdUtils from '@/mixins/IdUtils.js'
 
-  import WebHankoContainer from '@/components/web_hanko/WebHankoContainer.vue'
-  import EmailButton from '@/components/EmailButton.vue'
-  import PdfViewer from '@/components/PdfViewer.vue'
-  import RecipientComments from '@/components/RecipientComments.vue'
-  import AddGroupDialog from '@/components/AddGroupDialog.vue'
+import WebHankoContainer from '@/components/web_hanko/WebHankoContainer.vue'
+import EmailButton from '@/components/EmailButton.vue'
+import PdfViewer from '@/components/PdfViewer.vue'
+import RecipientComments from '@/components/RecipientComments.vue'
+import AddGroupDialog from '@/components/AddGroupDialog.vue'
 
-  export default {
-    name: 'Application',
+import {
+  generate_email_to_recipient,
+  generate_email_to_applicant,
+} from '@/emails'
 
-    components: {
-      WebHankoContainer,
-      PdfViewer,
-      RecipientComments,
-      EmailButton,
-      HelpDialog,
-      AddGroupDialog,
-    },
-    mixins: [
-      IdUtils
-    ],
-    data(){
-      return {
-        help_dialog: false,
-        application: null,
-        loading: false,
-        error: null,
-        selected_file_id: null
+export default {
+  name: 'Application',
 
-      }
-    },
-    mounted(){
-      this.get_application()
-    },
-    beforeRouteLeave(to, from, next) {
-      const email_required = this.$store.state.email_required
-      if(email_required){
-        if(confirm(`メール未送信なのにページから出ますか？`)) {
-          this.$store.commit('require_email', false)
-          next()
-        }
-      }
-      else next()
-    },
-    methods: {
-      get_application(){
-        this.loading = true
-        this.application = null
-        this.error = null
-        const url = `/v2/applications/${this.application_id}`
-        this.axios.get(url)
-        .then(({data}) => {
-          this.application = data
-
-          // parsing form_data back into JSON because Neo4J cannot store JSON directly
-          if(!this.application.form_data) return
-          this.application.form_data = JSON.parse(this.application.form_data)
-
-        })
-        .catch((error) => {
-          if(error.response) {
-            console.error(error.response.data)
-            if(error.response.status === 404) {
-              this.error = `アイテム${this.application_id}見つけれませんでした
-              Item ${this.application_id} not found`
-            }
-          }
-          else {
-            console.error(error)
-          }
-        })
-        .finally(() => {
-          this.loading = false
-        })
-      },
-      view_pdf (file_id) {
-        this.selected_file_id = file_id
-      },
-      approve_application(){
-        if(!confirm(`承認しますか? / Approve application?`)) return
-        const url = `/v2/applications/${this.application_id}/approve`
-        this.axios.post(url)
-        .then(() => { this.get_application() })
-        .catch((error) => {
-          console.error(error)
-          alert(`Error approving application`)
-        })
-      },
-      reject_application(){
-        if(!confirm(`却下しますか? / Reject application?`)) return
-        const url = `/v2/applications/${this.application_id}/reject`
-
-        this.axios.post(url)
-        .then(() => { this.get_application() })
-        .catch((error) => {
-          console.error(error)
-          alert(`Error rejecting application`)
-        })
-      },
-      delete_application(){
-        if(!confirm("本申請を削除致しますか？")) return
-        const url = `/v2/applications/${this.application_id}`
-        this.axios.delete(url)
-        .then( () => {
-          this.$router.push({name: 'submitted_applications'})
-        })
-        .catch((error) => {
-          if(error.response) console.error(error.response.data)
-          else console.error(error)
-        })
-      },
-      format_date_neo4j(date){
-        return `${date.year}/${date.month}/${date.day}`
-      },
-      email_button_clicked(){
-
-        if(this.current_recipient) this.send_email_to_recipient(this.current_recipient)
-        else this.send_email_to_applicant()
-      },
-
-      send_email_to_recipient (recipient) {
-
-        this.$store.commit('require_email', false)
-
-        // Weird formatting because preserves indentation
-
-        const email_body = `${recipient.display_name} 様
-
-申請マネージャーの通知メールです。
-
-申請を提出しました。
-
-申請者: ${this.application.applicant.display_name}
-タイプ: ${this.application.type}
-タイトル: ${this.application.title}
-提出先URL: ${window.location.origin}/applications/${this.get_id_of_item(this.application)}
-
-※IEでは動作しません。Edge (Chromium)/Firefox/GoogleChromeをご使用ください。
-※詳しくは ${window.location.origin}/info
-
-確認お願いします。`
-
-      const email_string = `mailto:${recipient.email_address}
-?subject=[申請マネージャ] ${this.application.type}
-&body=${encodeURIComponent(email_body)}`
-
-        window.location.href = email_string
-
-      },
-      send_email_to_applicant () {
-
-        this.$store.commit('require_email', false)
-        // Weird formatting because preserves indentation
-
-        const email_body = `${this.application.applicant.display_name} 様
-
-申請マネージャーの通知メールです。
-
-申請の承認が${this.application_is_rejected ? '却下' : '完了'}されました。
-
-申請者: ${this.application.applicant.display_name}
-タイプ: ${this.application.type}
-タイトル: ${this.application.title}
-提出先URL: ${window.location.origin}/applications/${this.get_id_of_item(this.application)}
-
-※IEでは動作しません。Edge (Chromium)/Firefox/GoogleChromeをご使用ください。
-※詳しくは ${window.location.origin}/info
-
-確認お願いします。`
-
-        const email_string = `mailto:${this.application.applicant.email_address}
-  ?subject=[申請マネージャ] ${this.application.type}
-  &body=${encodeURIComponent(email_body)}`
-
-        window.location.href = email_string
-      },
-      update_privacy_of_application () {
-        const url = `/v2/applications/${this.application_id}/privacy`
-        const body = { private: this.application.private }
-        this.axios.put(url, body)
-          .then(() => { })
-          .catch(() => alert('Error updating privacy of application'))
-      },
-      share_with_group (group) {
-        const url = `/v2/applications/${this.application_id}/privacy/groups`
-        const body = { group_id: this.get_id_of_item(group) }
-        this.axios.post(url, body)
-          .then(() => {
-            this.get_application()
-          })
-          .catch((error) => {
-            alert('Error updating visibility of application')
-            console.error(error)
-          })
-      },
-      remove_application_visibility_to_group (group) {
-        const url = `/v2/applications/${this.application_id}/privacy/groups/${this.get_id_of_item(group) }`
-        this.axios.delete(url)
-          .then(() => {
-            this.get_application()
-          })
-          .catch((error) => {
-            console.error(error)
-            alert('Error updating visibility of application')
-          })
-      },
-      download_attachment(file_id) {
-
-        const url = `${process.env.VUE_APP_SHINSEI_MANAGER_URL}/v2/applications/${this.application_id}/files/${file_id}`
-        window.open(url,'_blank')
-      },
-      user_has_stamped_attachment (file_id) {
-
-        if(!this.user_as_recipient) return false
-
-        const found_approval = this.user_as_recipient.approval
-
-        if (!found_approval) return
-
-        let attachment_hankos = found_approval.attachment_hankos
-
-        if(typeof attachment_hankos === 'string'){
-          try {  attachment_hankos = JSON.parse(attachment_hankos)  }
-          catch (e) {  console.error('Failed to parse attachment hankos') }
-        }
-
-        if(!attachment_hankos) return
-
-        return !!attachment_hankos.find(a => a.file_id === file_id)
-
-      },
-
-    },
-    computed: {
-      application_id(){
-        return this.$route.params.application_id
-      },
-      ordered_recipients(){
-        return this.application.recipients
-          .slice()
-          .sort((a, b) => b.submission.flow_index - a.submission.flow_index)
-      },
-      current_recipient(){
-        // recipients sorted by flow index apparently
-        if(this.application.recipients.find(recipient => recipient.refusal)) return null
-
-        return this.application.recipients
-        .slice()
-        .sort((a, b) => a.submission.flow_index - b.submission.flow_index)
-        .find(recipient => !recipient.approval && !recipient.refusal)
-      },
-      user_as_recipient(){
-        return this.application.recipients.find(recipient => this.get_id_of_item(recipient) === this.current_user_id)
-      },
-      application_is_rejected(){
-        return !!this.application.recipients.find(recipient => recipient.refusal)
-      },
-      application_is_fully_approved(){
-        const recipient_count = this.application.recipients.length
-        const approval_count = this.application.recipients.reduce((acc, recipient) => acc + (recipient.approval ? 1 : 0), 0)
-        return approval_count === recipient_count
-      },
-      user_is_applicant () {
-        return this.get_id_of_item(this.application.applicant) === this.current_user_id
-      },
-      current_recipient_is_current_user(){
-        if(!this.current_recipient) return false
-        const current_recipient_id = this.get_id_of_item(this.current_recipient)
-        return current_recipient_id === this.current_user_id
-      },
-      approvals() {
-        return this.application.recipients
-          .filter(a => !!a.approval)
-          .map(r => r.approval)
-      },
-
-
+  components: {
+    WebHankoContainer,
+    PdfViewer,
+    RecipientComments,
+    EmailButton,
+    HelpDialog,
+    AddGroupDialog,
+  },
+  mixins: [
+    IdUtils
+  ],
+  data(){
+    return {
+      help_dialog: false,
+      application: null,
+      loading: false,
+      error: null,
+      selected_file_id: null
 
     }
+  },
+  mounted(){
+    this.get_application()
+  },
+  beforeRouteLeave(to, from, next) {
+    const email_required = this.$store.state.email_required
+    if(email_required){
+      if(confirm(`メール未送信なのにページから出ますか？`)) {
+        this.$store.commit('require_email', false)
+        next()
+      }
+    }
+    else next()
+  },
+  methods: {
+    get_application(){
+      this.loading = true
+      this.application = null
+      this.error = null
+      const url = `/v2/applications/${this.application_id}`
+      this.axios.get(url)
+      .then(({data}) => {
+        this.application = data
+
+        // parsing form_data back into JSON because Neo4J cannot store JSON directly
+        if(!this.application.form_data) return
+        this.application.form_data = JSON.parse(this.application.form_data)
+
+      })
+      .catch((error) => {
+        if(error.response) {
+          console.error(error.response.data)
+          if(error.response.status === 404) {
+            this.error = `アイテム${this.application_id}見つけれませんでした
+            Item ${this.application_id} not found`
+          }
+        }
+        else {
+          console.error(error)
+        }
+      })
+      .finally(() => {
+        this.loading = false
+      })
+    },
+    view_pdf (file_id) {
+      this.selected_file_id = file_id
+    },
+    approve_application(){
+      if(!confirm(`承認しますか? / Approve application?`)) return
+      const url = `/v2/applications/${this.application_id}/approve`
+      this.axios.post(url)
+      .then(() => { this.get_application() })
+      .catch((error) => {
+        console.error(error)
+        alert(`Error approving application`)
+      })
+    },
+    reject_application(){
+      if(!confirm(`却下しますか? / Reject application?`)) return
+      const url = `/v2/applications/${this.application_id}/reject`
+
+      this.axios.post(url)
+      .then(() => { this.get_application() })
+      .catch((error) => {
+        console.error(error)
+        alert(`Error rejecting application`)
+      })
+    },
+    delete_application(){
+      if(!confirm("本申請を削除致しますか？")) return
+      const url = `/v2/applications/${this.application_id}`
+      this.axios.delete(url)
+      .then( () => {
+        this.$router.push({name: 'submitted_applications'})
+      })
+      .catch((error) => {
+        if(error.response) console.error(error.response.data)
+        else console.error(error)
+      })
+    },
+    format_date_neo4j(date){
+      return `${date.year}/${date.month}/${date.day}`
+    },
+    email_button_clicked(){
+
+      if(this.current_recipient) this.send_email_to_recipient(this.current_recipient)
+      else this.send_email_to_applicant()
+    },
+
+    send_email_to_recipient (recipient) {
+      this.$store.commit('require_email', false)
+      window.location.href = generate_email_to_recipient(this.application, recipient)
+    },
+    send_email_to_applicant () {
+      this.$store.commit('require_email', false)
+      window.location.href = generate_email_to_applicant(this.application)
+    },
+    update_privacy_of_application () {
+      const url = `/v2/applications/${this.application_id}/privacy`
+      const body = { private: this.application.private }
+      this.axios.put(url, body)
+        .then(() => { })
+        .catch(() => alert('Error updating privacy of application'))
+    },
+    share_with_group (group) {
+      const url = `/v2/applications/${this.application_id}/privacy/groups`
+      const body = { group_id: this.get_id_of_item(group) }
+      this.axios.post(url, body)
+        .then(() => {
+          this.get_application()
+        })
+        .catch((error) => {
+          alert('Error updating visibility of application')
+          console.error(error)
+        })
+    },
+    remove_application_visibility_to_group (group) {
+      const url = `/v2/applications/${this.application_id}/privacy/groups/${this.get_id_of_item(group) }`
+      this.axios.delete(url)
+        .then(() => {
+          this.get_application()
+        })
+        .catch((error) => {
+          console.error(error)
+          alert('Error updating visibility of application')
+        })
+    },
+    download_attachment(file_id) {
+
+      const url = `${process.env.VUE_APP_SHINSEI_MANAGER_URL}/v2/applications/${this.application_id}/files/${file_id}`
+      window.open(url,'_blank')
+    },
+    user_has_stamped_attachment (file_id) {
+
+      if(!this.user_as_recipient) return false
+
+      const found_approval = this.user_as_recipient.approval
+
+      if (!found_approval) return
+
+      let attachment_hankos = found_approval.attachment_hankos
+
+      if(typeof attachment_hankos === 'string'){
+        try {  attachment_hankos = JSON.parse(attachment_hankos)  }
+        catch (e) {  console.error('Failed to parse attachment hankos') }
+      }
+
+      if(!attachment_hankos) return
+
+      return !!attachment_hankos.find(a => a.file_id === file_id)
+
+    },
+
+  },
+  computed: {
+    application_id(){
+      return this.$route.params.application_id
+    },
+    ordered_recipients(){
+      return this.application.recipients
+        .slice()
+        .sort((a, b) => b.submission.flow_index - a.submission.flow_index)
+    },
+    current_recipient(){
+      // recipients sorted by flow index apparently
+      if(this.application.recipients.find(recipient => recipient.refusal)) return null
+
+      return this.application.recipients
+      .slice()
+      .sort((a, b) => a.submission.flow_index - b.submission.flow_index)
+      .find(recipient => !recipient.approval && !recipient.refusal)
+    },
+    user_as_recipient(){
+      return this.application.recipients.find(recipient => this.get_id_of_item(recipient) === this.current_user_id)
+    },
+    application_is_rejected(){
+      return !!this.application.recipients.find(recipient => recipient.refusal)
+    },
+    application_is_fully_approved(){
+      const recipient_count = this.application.recipients.length
+      const approval_count = this.application.recipients.reduce((acc, recipient) => acc + (recipient.approval ? 1 : 0), 0)
+      return approval_count === recipient_count
+    },
+    user_is_applicant () {
+      return this.get_id_of_item(this.application.applicant) === this.current_user_id
+    },
+    current_recipient_is_current_user(){
+      if(!this.current_recipient) return false
+      const current_recipient_id = this.get_id_of_item(this.current_recipient)
+      return current_recipient_id === this.current_user_id
+    },
+    approvals() {
+      return this.application.recipients
+        .filter(a => !!a.approval)
+        .map(r => r.approval)
+    },
+
+
+
   }
+}
 </script>
 
 <style>
