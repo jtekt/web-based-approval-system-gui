7bf1ba9b11d4506568ec92d456a2a7babade217a
Maxime Moreillon <m.moreillon@gmail.com>
2022-11-10 14:34:56 +0900

    logo, header

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,12 @@ export default {
         login_url: process.env.VUE_APP_LOGIN_URL,
         identification_url: process.env.VUE_APP_IDENTIFICATION_URL,
         password_reset_url: process.env.VUE_APP_PASSWORD_RESET_URL,
+
+        header_logo: require('@/assets/jtekt_logo_negative.jpg'),
+        authentication_logo: require('@/assets/jtekt_logo.jpg'),
+        colors: { app_bar: '#000' },
+        author: 'JTEKT Corporation',
+
       },
 
     }
@@ -78,3 +84,9 @@ export default {
 
 }
 </script>
+
+<style>
+.header_logo {
+  border-right: 1px solid white;
+}
+</style>
