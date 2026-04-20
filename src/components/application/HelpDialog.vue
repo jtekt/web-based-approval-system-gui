8ce581f1375541bb2bf4ac9a70293e3829ac5eeb
Kevin Fabel <kevin_fabel@jtekt.co.jp>
2026-04-20 09:54:02 +0900

    Translate help

--- src/components/application/HelpDialog.vue
+++ src/components/application/HelpDialog.vue
@@ -3,46 +3,46 @@
     <template #activator="{ props }">
       <v-btn variant="text" v-bind="props">
         <v-icon start>mdi-information</v-icon>
-        <span>Help</span>
+        <span>{{ t('help.button') }}</span>
       </v-btn>
     </template>
 
     <v-card>
-      <v-card-title class="text-h4">Help</v-card-title>
+      <v-card-title class="text-h4">
+        {{ t('help.title') }}
+      </v-card-title>
 
-      <v-card-subtitle class="mt-4 text-h6">機能</v-card-subtitle>
-      <v-card-text>添付のPDFに捺印（サイズ変更も可能）が可能です。</v-card-text>
+      <v-card-subtitle class="mt-4 text-h6">
+        {{ t('help.features.title') }}
+      </v-card-subtitle>
+      <v-card-text>
+        {{ t('help.features.description') }}
+      </v-card-text>
 
-      <v-card-subtitle class="mt-4 text-h6">回付手順</v-card-subtitle>
+      <v-card-subtitle class="mt-4 text-h6">
+        {{ t('help.steps.title') }}
+      </v-card-subtitle>
       <v-card-text>
         <v-list density="compact">
-          <v-list-item
-            >１．ページ下部に表示されているPDF上の任意の場所に捺印します。（サイズ変更可能）</v-list-item
-          >
-          <v-list-item
-            >２．メール作成ボタンを押すと、自動で次の回付者へメールが作成されます。</v-list-item
-          >
-          <v-list-item
-            >３．作成されたメール（Outlook）をご自身で送信して下さい。</v-list-item
-          >
-          <v-list-item
-            >４．以上で回付作業は終了です。ブラウザの×のボタンで閉じて下さい。</v-list-item
-          >
+          <v-list-item>{{ t('help.steps.step1') }}</v-list-item>
+          <v-list-item>{{ t('help.steps.step2') }}</v-list-item>
+          <v-list-item>{{ t('help.steps.step3') }}</v-list-item>
+          <v-list-item>{{ t('help.steps.step4') }}</v-list-item>
         </v-list>
       </v-card-text>
 
-      <v-card-subtitle class="mt-4 text-h6"
-        >捺印済みファイルのダウンロード手順</v-card-subtitle
-      >
+      <v-card-subtitle class="mt-4 text-h6">
+        {{ t('help.download.title') }}
+      </v-card-subtitle>
       <v-card-text>
-        ページに表示されているPDF上部にあるダウンロードから捺印済みのファイルをダウンロードします。
+        {{ t('help.download.description') }}
       </v-card-text>
 
       <v-card-actions>
         <v-spacer />
-        <v-btn variant="text" color="primary" @click="dialog = false"
-          >Close</v-btn
-        >
+        <v-btn variant="text" color="primary" @click="dialog = false">
+          {{ t('Close') }}
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -50,6 +50,8 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 const dialog = ref(false)
+const { t } = useI18n()
 </script>
