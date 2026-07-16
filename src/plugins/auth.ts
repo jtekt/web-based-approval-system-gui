e2384fa73f6cac7eef375d9ceffdf5b1ff38dd93
Kevin Fabel <kevin_fabel@jtekt.co.jp>
2026-07-16 13:46:59 +0900

    Update @jtekt/vuetify-auth to version 0.3.1

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -6,12 +6,12 @@ export const auth = createAuthPlugin(
     oidc: {
       clientId: import.meta.env.VITE_OIDC_CLIENT_ID!,
       authority: import.meta.env.VITE_OIDC_AUTHORITY!,
-      enrichmentEndpoint: import.meta.env.VITE_LEGACY_IDENTIFICATION_URL!,
+      enrichmentEndpoint: import.meta.env.VITE_LEGACY_IDENTIFICATION_URL,
       identifierLookupField: '_id',
     },
     credentials: {
       loginEndpoint: import.meta.env.VITE_LEGACY_LOGIN_URL!,
-      resetPasswordEndpoint: import.meta.env.VITE_LEGACY_PASSWORD_RESET_URL!,
+      resetPasswordEndpoint: import.meta.env.VITE_LEGACY_PASSWORD_RESET_URL,
     },
   },
   router
